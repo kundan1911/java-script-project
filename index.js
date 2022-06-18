a2cb259f5ad7d50d46ad2bb4125c2bf12789029c
kundan1911 <83800542+kundan1911@users.noreply.github.com>
2022-06-18 16:12:56 +0530

    Add files via upload
    Simon game

--- index.js
+++ index.js
@@ -1,22 +1,91 @@
 
-var a=Math.random();
-var b=Math.random();
-a*=6;
-b*=6;
-a=Math.floor(a);
-b=Math.floor(b);
-a++;
-b++;
+        var level=1; 
+        var pattern=""
+        var userPressed=""
+        var a=0
+$(document).keypress(function(){
+        if(userPressed.length===0 && pattern.length===0){
+        $('.heading').text("level "+level) ;
+        pattern_addition();
+        }
+})
 
-if(a===b){
-document.querySelector(".heading").textContent="Draw!"
+$("button").click(function(event){
+    var selectButton=event.currentTarget.attributes.id.value
+press(selectButton)
+  
+})
+function displayButton(val){
+    switch (val) {
+        case 1:
+        return "a";
+        case 2:
+        return "b";
+        case 3:
+        return "c";
+        case 4:
+        return "d";
+        
+    }
 }
-else if(a>b){
-    document.querySelector(".heading").innerHTML="<img src='/dicegame/images/flag.png' class='flag'> Player 1 Wins! "
-}
-else{
-    document.querySelector(".heading").innerHTML="Player 2 Wins! <img src='/dicegame/images/flag.png' class='flag'> "
-}  
+function press(sb){
+    shadowSound(sb);
+    userPressed+=sb;
+    if(userPressed.length <= pattern.length){
+        if(userPressed[a]===pattern[a]){
+            if(a<pattern.length-1){
+                
+                a++
+            }
+            else{
+                level++;
+                nextlevel(level);
+                userPressed=""
+                a=0;
+                pattern_addition();
+            }
+        }
+        else{
+            abort()
+        }
+    }
+    else{
+        abort();
+    }
+    }
 
-document.querySelector(".p1").setAttribute("src","/dicegame/images/"+a.toString()+".png ")
-document.querySelector(".p2").setAttribute("src","/dicegame/images/"+b.toString()+".png ")
+function nextlevel(lev){
+    $('.heading').text("Level"+lev);
+}
+function soundDisplay(sb){
+    $("#"+sb).fadeOut().fadeIn();
+    var sound=new Audio("/sounds/tom-"+sb+".mp3")
+    sound.play();
+    
+}
+function shadowSound(sb){
+    $("#"+sb).addClass("pressed")
+    setTimeout(function(){
+        $("#"+sb).removeClass("pressed")
+    },100)
+    var sound=new Audio("/sounds/tom-"+sb+".mp3")
+    sound.play();
+}
+function pattern_addition(){
+    var buttonValue=Math.floor((Math.random()*4))+1;
+    var b=displayButton(buttonValue)
+    pattern+=b;
+    soundDisplay(b);
+}
+function abort(){
+    $('.heading').text("Game Over, Press Any Key to Restart")
+    $('body').addClass("endgame");
+    setTimeout(function(){
+        $('body').removeClass("endgame")
+    },100)
+    var sound=new Audio("/sounds/crash.mp3")
+    sound.play()
+    a=0;
+    userPressed=""
+    pattern=""
+    level=1}
